f12fd129c08e06174b76d8229d5320b98f492ec0
Sercan Haydaroglu <48677717+shaydaroglu@users.noreply.github.com>
2026-03-25 01:12:32 +0100

    Creates domain model
    - Adds Device record
    - Adds DeviceState enum

---
+++ src/main/java/com/sercan/device_service/device/domain/model/Device.java
@@ -0,0 +1,38 @@
+package com.sercan.device_service.device.domain.model;
+
+import org.apache.logging.log4j.util.Strings;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record Device(
+        UUID id,
+        String name,
+        String brand,
+        DeviceState state,
+        Instant createdAt,
+        Instant updatedAt
+) {
+    public Device {
+        if (Strings.isBlank(name)) {
+            throw new IllegalArgumentException("Device name cannot be null or blank.");
+        }
+        if (Strings.isBlank(brand)) {
+            throw new IllegalArgumentException("Device brand cannot be null or blank.");
+        }
+        if (state == null) {
+            throw new IllegalArgumentException("Device state cannot be null.");
+        }
+    }
+
+    public static Device newDevice(String name, String brand, DeviceState state) {
+        return new Device(
+                UUID.randomUUID(),
+                name,
+                brand,
+                state == null ? DeviceState.INACTIVE : state,
+                null,
+                null
+        );
+    }
+}
---
+++ src/main/java/com/sercan/device_service/device/domain/model/DeviceState.java
@@ -0,0 +1,7 @@
+package com.sercan.device_service.device.domain.model;
+
+public enum DeviceState {
+    AVAILABLE,
+    IN_USE,
+    INACTIVE
+}
